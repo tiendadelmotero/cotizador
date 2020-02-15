eedc818edf40eb8dbd3d36746a615eb48552bd67
tiendadelmotero <61068635+tiendadelmotero@users.noreply.github.com>
2020-02-14 20:25:37 -0500

    Update resultado.js

--- js/resultado.js
+++ js/resultado.js
@@ -18,14 +18,30 @@
 
 $(document).on("submit","#precio",function(e){
     e.preventDefault();
+    //        
+        var varmarca = document.getElementById("marca"); 
+        var varreferencia = document.getElementById("referencia");
+        
+        var varmodelo = document.getElementById("modelo");
+
+        var varsoat = document.getElementById("soat");
+
+        var varcorreo = document.getElementById("correo");
+    
+        var vartelefono = document.getElementById("telefono");
+
+        var varcheck = document.getElementById("exampleCheck1");
+    //
+    if(varmarca.value==0 || varmodelo.value==0 || varsoat.value==0 || varcorreo.value==0 || vartelefono.value==0 || varcheck.value==0 ){
+    
+        document.getElementById('res').style.height='20px';}
+    else{
     document.getElementById('res').style.height='200px';
-    document.getElementById('res').style.radius='10px';
-    document.getElementById('res').style.background='2px solid #FFF';
-    document.getElementById('contenedor').style.height='910px';
+    document.getElementById('res').style.radius='10px'; 
+    document.getElementById('res').style.background='2px solid #FFF';}
+    document.getElementById('contenedor').style.height='950px';
     
     $.post("precio.php",$("#precio").serialize(),function(res){
         $("#cargapost").html(res);
     });
 })
-
-/* border-radius: 10px; */
